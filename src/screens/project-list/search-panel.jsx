fc5898752f2a9a5ac41f90a32514d9acc9d85325
csz5302017 <18201796637@163.com>
2021-12-08 19:31:02 +0800

    test

--- src/screens/project-list/search-panel.jsx
+++ src/screens/project-list/search-panel.jsx
@@ -1,23 +1,23 @@
 
 export const SearchPanel = () => {
-	useEffect(() => {
-		fetch('').then(response => {
-			
-		})
-	}, [input])
-	const [state, setState] = useState(0)
-	const [list, setList] = useState(0)
+	// useEffect(() => {
+	// 	fetch('').then(response => {
+
+	// 	})
+	// }, [input])
+	// const [state, setState] = useState(0)
+	// const [list, setList] = useState(0)
 	return (
 		<form action="">
 			<input />
-			<select>
+			{/* <select>
 				<option>负责人</option>
 				{list.map(item => {
 					return (
 						<option>{item}</option>
 					)
 				})}
-			</select>
+			</select> */}
 		</form>
 	)
 }
